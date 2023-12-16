4ba54ed8c916efaa0486dbb24a15f58b7d6e2c11
incognitotgt <mashoor539@icloud.com>
2023-12-16 10:50:36 -0500

    CSS made slightly better

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,5 +1,6 @@
 import './index.css';
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGear,faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 let page;
 function determinePage () {
     if (window.location.pathname === '/') {
@@ -15,10 +16,10 @@ export function NavBar() {
     if (page === 'home') {
         return (
             <>
-                <div className="Rectangle2 fixed top-5 left-5 right-5 h-32 opacity-75 bg-neutral-900 rounded-2xl" />
-                <ul>
-                    <li><a href="/settings">Settings</a></li>
-                    <li><a href="">open about:blank</a></li>
+                <nav className="Rectangle2 fixed top-5 left-5 right-5 h-32 opacity-75 bg-neutral-900 rounded-2xl flex items-center px-57" />
+                <ul className="flex justify-around w-full items-center">
+                    <li><a href="/settings"><FontAwesomeIcon icon={faGear} style={{color: "#ffffff",}}className="fixed w-[35px] h-[38px] left-57"/></a></li>
+                    <li><a href="#"><FontAwesomeIcon icon={faArrowUpRightFromSquare} style={{color: "#ffffff",}} /></a></li>
                     
                 </ul>
             </>
@@ -29,7 +30,6 @@ export function NavBar() {
             <div id='nav'>
                 <ul>
                     <li id="homeIcon"><a href="/">Home</a></li>
-                    <li><a href="">open about:blank</a></li>
                 </ul>
             </div>
             </>
