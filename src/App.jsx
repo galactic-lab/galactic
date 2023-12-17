d2c48cde4291d3ab5f8c2af3c3228a23db3ece95
incognitotgt <mashoor539@icloud.com>
2023-12-16 22:54:10 -0500

    e

--- src/App.jsx
+++ src/App.jsx
@@ -28,8 +28,7 @@ function App() {
       <div className="mt-8 sm:mt-12 lg:mt-16 relative flex justify-center">
         <div id="Glow" className="shadow-glow w-full max-w-xs sm:max-w-sm lg:max-w-md"/>
           <form id="urlForm" onSubmit={processRequest}>
-          <input id="address" placeholder="Browse the web freely" className="w-full max-w-xs sm:max-w-sm lg:max-w-md bg-[#181818] text-base text-[rgba(235, 235, 245, 0.6)] py-3 px-4 rounded-lg outline-none mt-8 sm:mt-12 lg:mt-16 relative flex justify-center"  />
-            </form>
+          <input id="address" placeholder="Browse the web freely" className="w-full max-w-xs sm:max-w-sm lg:max-w-md bg-[#181818] text-base text-[rgba(235,_235,_245,_0.6)] py-3 px-4 rounded-lg outline-none"/>            </form>
       </div>
 </>
 )
