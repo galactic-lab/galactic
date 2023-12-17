edafd7f8f05f6d815f9b3c88a8a976d8d5d4543e
incognitotgt <mashoor539@icloud.com>
2023-12-16 22:48:17 -0500

    Buggggyygygy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import './index.css';
 import { useEffect } from 'react'
-
-
+import { useNavigate } from "react-router-dom"
+function processRequest(event) {
+    event.preventDefault(); // Prevent the default form submit action
+    // Your request processing logic here
+    const [input, setInput] = useState('');
+  }
 function App() {
   useEffect(() => {
       if ('serviceWorker' in navigator) {
@@ -23,9 +27,9 @@ function App() {
       <>
       <div className="mt-8 sm:mt-12 lg:mt-16 relative flex justify-center">
         <div id="Glow" className="shadow-glow w-full max-w-xs sm:max-w-sm lg:max-w-md"/>
-          <form id="urlForm">
-        <input id="address" placeholder="Browse the web freely" className="w-full max-w-xs sm:max-w-sm lg:max-w-md bg-[#181818] text-base text-[rgba(235,_235,_245,_0.6)] py-3 px-4 rounded-lg outline-none"/>
-          </form>
+          <form id="urlForm" onSubmit={processRequest}>
+          <input id="address" placeholder="Browse the web freely" className="w-full max-w-xs sm:max-w-sm lg:max-w-md bg-[#181818] text-base text-[rgba(235, 235, 245, 0.6)] py-3 px-4 rounded-lg outline-none mt-8 sm:mt-12 lg:mt-16 relative flex justify-center"  />
+            </form>
       </div>
 </>
 )
