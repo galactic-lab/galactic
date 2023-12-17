1d2f59e45e4eab5c33cd01d8d752ba73d5e349e5
incognitotgt <mashoor539@icloud.com>
2023-12-16 20:54:28 -0500

    Service worker works

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { useEffect } from 'react'
 function App() {
   useEffect(() => {
       if ('serviceWorker' in navigator) {
-          navigator.serviceWorker.register(`/sw-register.js`, { scope: '/uv/' }).then(
+          navigator.serviceWorker.register(`/register.js`, { scope: '/uv/' }).then(
               () => {
                   console.log('Proccy service worker registered');
               },
@@ -24,7 +24,7 @@ function App() {
       <div className="mt-8 sm:mt-12 lg:mt-16 relative flex justify-center">
         <div id="Glow" className="shadow-glow w-full max-w-xs sm:max-w-sm lg:max-w-md"/>
           <form id="urlForm">
-        <input id="address" onClick="text" placeholder="Browse the web freely" className="w-full max-w-xs sm:max-w-sm lg:max-w-md bg-[#181818] text-base text-[rgba(235,_235,_245,_0.6)] py-3 px-4 rounded-lg outline-none"/>
+        <input id="address" placeholder="Browse the web freely" className="w-full max-w-xs sm:max-w-sm lg:max-w-md bg-[#181818] text-base text-[rgba(235,_235,_245,_0.6)] py-3 px-4 rounded-lg outline-none"/>
           </form>
       </div>
 </>
