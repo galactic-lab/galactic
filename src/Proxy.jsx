2dc566363003ddbbe25d65dc5ec3a39854e0f036
incognitotgt <mashoor539@icloud.com>
2023-12-19 20:08:54 -0500

    fjnrkf vf
    rvvgtb

--- src/Proxy.jsx
+++ src/Proxy.jsx
@@ -1,11 +1,21 @@
-import { useNavigate } from "react-router-dom";
-function RedirectToProccy() {
-    const navigate = useNavigate();
-    navigate('/view');
+import ReactDOM from 'react-dom';
+import { decodeXor } from '/public/tools.js';
+
+
+function Proccy()
+{
+    function IframeWithDecodedSource({ encodedSrc }) {
+    const decodedSrc = decodeXor(encodedSrc);
+
+  return <iframe src={decodedSrc} />;
 }
 
-function Proccy(encodedSrc) {
-    RedirectToProccy();
-    return (<iframe src={"/uv/" + encodedSrc} className="w-full h-screen"/>);
-}
+const encodedSrc = 'encoded_source_url';
+
+    ReactDOM.createRoot(document.getElementById('root')).render(
+    <>
+        <IframeWithDecodedSource />
+
+    </>
+)}
 export default Proccy
