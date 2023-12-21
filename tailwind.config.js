51963d056a3a722f9e6ba3c1a356f50602cb0741
incognitotgt <mashoor539@icloud.com>
2023-12-21 12:23:39 -0500

    Added DaisyUI sapport

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+import daisy from 'daisyui'
 export default {
     content: [
         './src/**/*.{js,jsx,ts,tsx}',
         './**/*.html'
     ],
+    plugins: [daisy],
     theme: {
         extend: {
             fontFamily: {
                 'lexend': ['Lexend']
         }
-    },
-    plugins: [],
+    }
     }
 }
