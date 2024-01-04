09716530811966da91461946b5fba1be92d54a35
incognitotgt <mashoor539@icloud.com>
2024-01-04 09:23:25 -0500

    Revert "Added DaisyUI sapport"
    This reverts commit 51963d056a3a722f9e6ba3c1a356f50602cb0741.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-import daisy from 'daisyui'
 export default {
     content: [
         './src/**/*.{js,jsx,ts,tsx}',
         './**/*.html'
     ],
-    plugins: [daisy],
     theme: {
         extend: {
             fontFamily: {
                 'inter': ['Inter']
         }
-    }
+    },
+    plugins: [],
     }
 }
